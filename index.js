e6c52599104de87127cd304724dc7ed3742fee1c
Cristian Andrade <cristian@appmelo.com>
2022-10-31 18:13:55 -0500

    first commit

--- index.js
+++ index.js
@@ -20,17 +20,18 @@ const m = async () => {
     // console.log(await sftpService.putFile({ ...config, remotePath: '/filePut.csv', bufferFile }));
     // ---- TESTING MOVII LOGIC ----
     // Getting csv array from server
-    const csvBufferFile = await sftpService.getFile({ ...config, remotePath: '/fileGet.csv' });
-    const csvArray = csvService.getArrayFromCSVBuffer(csvBufferFile, ';');
-    console.log(csvArray);
+    const csvBufferFile = await sftpService.getFile({ ...config, remotePath: '/fileGett.csv' });
+    // const csvArray = csvService.getArrayFromCSVBuffer(csvBufferFile, ';');
+    // console.log(csvArray);
+    console.log(csvBufferFile);
 
     // Putting csv from array
-    const arrayToPut = [
-      { c1: '1', c2: '2', c3: '3' },
-      { c1: '4', c2: '5', c3: '6' }
-    ];
-    const csvArrayPutBuffer = csvService.getCSVBufferFromArray(arrayToPut, ';');
-    await sftpService.putFile({ ...config, remotePath: '/filePutArray.csv', bufferFile: csvArrayPutBuffer });
+    // const arrayToPut = [
+    //   { c1: '1', c2: '2', c3: '3' },
+    //   { c1: '4', c2: '5', c3: '6' }
+    // ];
+    // const csvArrayPutBuffer = csvService.getCSVBufferFromArray(arrayToPut, ';');
+    // await sftpService.putFile({ ...config, remotePath: '/filePutArray.csv', bufferFile: csvArrayPutBuffer });
   } catch (error) {
     console.log(error);
   }
